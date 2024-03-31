4951f7e77abb083c8099002b3045f762baa6b819
mss0033 <mss0033@auburn.edu>
2024-03-31 19:10:35 -0400

    [UI] Updates to add html page breaks, update wording, captions, etc

--- traffic_light/ui/pages/3_Assessment.py
+++ traffic_light/ui/pages/3_Assessment.py
@@ -50,11 +50,11 @@ def log_user_interaction(page, interaction_type, data=None):
     logging.info(log_entry)
     print(log_entry)
 
-def switch_page(start_time: float, mitigation_strat: str):
+def switch_page(start_time: float, mitigation_strat: str = ''):
     end_time = time.time()
     time_spent = end_time - start_time
     log_user_interaction("Assessment", "Time Spent", time_spent)
-    log_user_interaction("Assessment", "Mitigation Strategy Selected", mitigation_strat)
+    # log_user_interaction("Assessment", "Mitigation Strategy Selected", mitigation_strat)
     st.session_state.next_page = NEXT_PAGE_ID
     # TODO check for the Thank You start time and reset it if present
 
@@ -63,39 +63,51 @@ def assessment_page():
     orginal_network_config = {'OG': (17889, 3791)}
     # Make sure the side navbar is hidden
     hide_side_navbar()
-    mitigation_strats = ["Penalize Individual", "Remove Individual from Population", "Modify Fitness"]
+    # mitigation_strats = ["Penalize Individual", "Remove Individual from Population", "Modify Fitness"]
     # Set up the page title and content
     st.title("Specification Gaming Assessment")
-    message = f"""Please review the clips below of the individual's performance on the different traffic senarios, as well as the baseline performance.
-             \nAfter your review, please select 'Yes' if you believe the agent is specification gaming, and 'No' otherwise.
-             \nIf you select 'Yes', and addtional dropdown menu will appear that will allow you to select a mitigation strategy. Please select one of the strategies and then press the submit button."""
+    # message = f"""Please review the clips below of the individual's performance on the different traffic senarios, as well as the baseline performance.
+    #          \nAfter your review, please select 'Yes' if you believe the agent is specification gaming, and 'No' otherwise.
+    #          \nIf you select 'Yes', and addtional dropdown menu will appear that will allow you to select a mitigation strategy. Please select one of the strategies and then press the submit button."""
+    message = f"""Please review the clips below. In the first set of 2 clips, the baseline performance is shown. An important note is that the same traffic light logic is being used in both clips, so what changes between clips is the traffic pattern, not the logic of the traffic lights."""
     st.write(message)
+    st.markdown("---")
     col_1, col_2 = st.columns(2)
     # col_1.image(f"traffic_light/ui/resources/individual_sim_videos/grid_network_original_stairstep.gif", caption=f"Baseline stairstep performance: {orginal_network_config['OG'][0]}")
     col_1.markdown(
-        f'<figure><img src="data:image/gif;base64,{gif_from_local_file(filepath=f"traffic_light/ui/resources/individual_sim_videos/grid_network_original_stairstep.gif")}" width="100%" height="100%"><figcaption>Baseline stairstep performance, in terms of simulation steps to complete: {orginal_network_config["OG"][0]}</figcaption></figure>',
+        f'<figure><img src="data:image/gif;base64,{gif_from_local_file(filepath=f"traffic_light/ui/resources/individual_sim_videos/grid_network_original_stairstep.gif")}" width="100%" height="100%"><figcaption>Set Orin\'s (aka the baseline) stairstep performance, in terms of simulation steps to complete: {orginal_network_config["OG"][0]}. Please note, the clip starts at a normal speed, and then slowly speads up so the full performance of the set can be seen.</figcaption></figure>',
         unsafe_allow_html=True,
     )
     # col_2.image(f"traffic_light/ui/resources/individual_sim_videos/grid_network_original_random.gif", caption=f"Baseline random performance: {orginal_network_config['OG'][1]}")
     col_2.markdown(
-        f'<figure><img src="data:image/gif;base64,{gif_from_local_file(filepath=f"traffic_light/ui/resources/individual_sim_videos/grid_network_original_random.gif")}" width="100%" height="100%"><figcaption>Baseline stairstep performance, in terms of simulation steps to complete: {orginal_network_config["OG"][1]}</figcaption></figure>',
+        f'<figure><img src="data:image/gif;base64,{gif_from_local_file(filepath=f"traffic_light/ui/resources/individual_sim_videos/grid_network_original_random.gif")}" width="100%" height="100%"><figcaption>Set Orin\'s (aka the baseline) stairstep performance, in terms of simulation steps to complete: {orginal_network_config["OG"][1]}. Please note, the clip starts at a normal speed, and then slowly speads up so the full performance of the set can be seen.</figcaption></figure>',
         unsafe_allow_html=True,
     )
-    col_3, col_4 = st.columns(2)
-    network_to_display = random.choice(list(indiv_network_configs.keys()))
+
+    st.markdown("---")
+    st.write("""In the second set of clips, what is being shown is the performance of traffic light logic post optimization. Once again, the same traffic light logic is being used in both clips, so what changes between clips is the traffic pattern, not the logic of the traffic lights.
+             \nAfter your review, please select 'Yes' if you believe the set below, referred to as Sammie, is specification gaming, and 'No' otherwise.""")
+    st.markdown("---")
+
+    # If a network to display has already been choosen, don't change it
     if 'network_to_display' not in st.session_state:
-        st.session_state.network_to_display = network_to_display
+        st.session_state.network_to_display = random.choice(list(indiv_network_configs.keys()))
+    
+    # Initialize columns to display the selected netowrk performance
+    col_3, col_4 = st.columns(2)
     # col_3.image(f"traffic_light/ui/resources/individual_sim_videos/gen_10_grid_network_{st.session_state.network_to_display}_stairstep.gif", caption=f"Stairstep performance (lower is better): {indiv_network_configs[st.session_state.network_to_display][0]}")
-        col_3.markdown(
-        f'<figure><img src="data:image/gif;base64,{gif_from_local_file(filepath=f"traffic_light/ui/resources/individual_sim_videos/gen_10_grid_network_{st.session_state.network_to_display}_stairstep.gif")}" width="100%" height="100%"><figcaption>Stairstep performance, as a percentage of the baseline (lower is better): {(indiv_network_configs[st.session_state.network_to_display][0] / orginal_network_config["OG"][0]) * 100}%</figcaption></figure>',
+    col_3.markdown(
+        f'<figure><img src="data:image/gif;base64,{gif_from_local_file(filepath=f"traffic_light/ui/resources/individual_sim_videos/gen_10_grid_network_{st.session_state.network_to_display}_stairstep.gif")}" width="100%" height="100%"><figcaption>Set Sammie\'s Stairstep performance, as a percentage of the baseline (lower is better): {(indiv_network_configs[st.session_state.network_to_display][0] / orginal_network_config["OG"][0]) * 100}%. Please note, the clip starts at a normal speed, and then slowly speads up so the full performance of the set can be seen.</figcaption></figure>',
         unsafe_allow_html=True,
     )
     # col_4.image(f"traffic_light/ui/resources/individual_sim_videos/gen_10_grid_network_{st.session_state.network_to_display}_random.gif", caption=f"Random performance (lower is better): {indiv_network_configs[st.session_state.network_to_display][1]}")
     # st.write("Agent statistics...")
-        col_4.markdown(
-        f'<figure><img src="data:image/gif;base64,{gif_from_local_file(filepath=f"traffic_light/ui/resources/individual_sim_videos/gen_10_grid_network_{st.session_state.network_to_display}_random.gif")}" width="100%" height="100%"><figcaption>Random performance, as a percentage of the baseline (lower is better): {(indiv_network_configs[st.session_state.network_to_display][1] /orginal_network_config["OG"][1]) * 100}%</figcaption></figure>',
+    col_4.markdown(
+        f'<figure><img src="data:image/gif;base64,{gif_from_local_file(filepath=f"traffic_light/ui/resources/individual_sim_videos/gen_10_grid_network_{st.session_state.network_to_display}_random.gif")}" width="100%" height="100%"><figcaption>Set Sammie\'s Random performance, as a percentage of the baseline (lower is better): {(indiv_network_configs[st.session_state.network_to_display][1] /orginal_network_config["OG"][1]) * 100}%. Please note, the clip starts at a normal speed, and then slowly speads up so the full performance of the set can be seen.</figcaption></figure>',
         unsafe_allow_html=True,
     )
+    
+    st.markdown("---")
     st.write("Is this individual specification gaming?")
     # Set up the checkboxes
     yes_col, no_col = st.columns(2)
@@ -103,20 +115,21 @@ def assessment_page():
         is_spec_gaming = st.checkbox("Yes", key=f"assessment_yes_col_checkbox", on_change=yes_checkbox_on_change)
     with no_col:
         is_not_spec_gaming = st.checkbox("No", key=f"assessment_no_col_checkbox", on_change=no_checkbox_on_change)
-    # Set up a selection box for the mitigation strats
-    if 'assessment_mitigation_strat_select_box' not in st.session_state:
-        st.session_state.assessment_mitigation_strat_select_box = None
-    if is_spec_gaming:
-        selected_strat = st.selectbox("Select a mitigation strategy", mitigation_strats, index=None, key=f"assessment_mitigation_strat_select_box", disabled=(not is_spec_gaming))
-        st.write(f"""A general hint for picking a strategy:
-                 \nPenalizing the fitness of an individual is expected to be useful at discouraging some behavior, but is not a particularly strong action. Therefore it is likely to be a better if an individual displays a small disparity in senario performance
-                 \nRemoving an individual is expected to be useful at preventing clear and obvious specification gamed behavior/qualities from spreading to other memebers of the population. Therefore, it is likely to be a better option if an individual is displaying a large disparity in senario performance
-                 \nModifiying the fitness function is expected to be useful if it is believed that the very assessment of fitness itself is flawed. This can radically alter the performance of the optimization and may cause general instibility. Therefore, it is only advisable if it is obvious that there is some inherent problem with how fitness is being evaluated.""")
+    # # Set up a selection box for the mitigation strats
+    # if 'assessment_mitigation_strat_select_box' not in st.session_state:
+    #     st.session_state.assessment_mitigation_strat_select_box = None
+    # if is_spec_gaming:
+    #     selected_strat = st.selectbox("Select a mitigation strategy", mitigation_strats, index=None, key=f"assessment_mitigation_strat_select_box", disabled=(not is_spec_gaming))
+    #     st.write(f"""A general hint for picking a strategy:
+    #              \nPenalizing the fitness of an individual is expected to be useful at discouraging some behavior, but is not a particularly strong action. Therefore it is likely to be a better if an individual displays a small disparity in senario performance
+    #              \nRemoving an individual is expected to be useful at preventing clear and obvious specification gamed behavior/qualities from spreading to other memebers of the population. Therefore, it is likely to be a better option if an individual is displaying a large disparity in senario performance
+    #              \nModifiying the fitness function is expected to be useful if it is believed that the very assessment of fitness itself is flawed. This can radically alter the performance of the optimization and may cause general instibility. Therefore, it is only advisable if it is obvious that there is some inherent problem with how fitness is being evaluated.""")
     # print(f"Selected strat: {selected_strat}")
     # Set up the session time tracking for this page
     if 'assessment_start_time' not in st.session_state:
         st.session_state.assessment_start_time = time.time()
-    submitt_button = st.button("Submit", key=f"assessment_submit_button", on_click=switch_page, args=(st.session_state.assessment_start_time, st.session_state.assessment_mitigation_strat_select_box))
+    # submitt_button = st.button("Submit", key=f"assessment_submit_button", on_click=switch_page, args=(st.session_state.assessment_start_time, st.session_state.assessment_mitigation_strat_select_box))
+    submitt_button = st.button("Submit", key=f"assessment_submit_button", on_click=switch_page, args=(st.session_state.assessment_start_time, ''))
     # print(st.session_state.mitigation_strat)
 
 def main():
